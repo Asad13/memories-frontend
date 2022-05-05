2350cc9b5a8eb9da35f3c7a56d903bc6466172fe
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-05-05 20:38:15 +0600

    Initial Setup and Appbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import memories from './images/memories.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth={false}>
+      <AppBar position='static' color='inherit' style={styles.appBar}>
+        <Typography variant='h2' component='h1' style={styles.heading}>Memories</Typography>
+        <img src={memories} alt='Memories' style={styles.image}/>
+      </AppBar>
+    </Container>
   );
+};
+
+const styles = {
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+  },
+  image: {
+    marginLeft: '15px',
+    maxWidth: '60px',
+    height: 'auto',
+  },
 }
 
 export default App;
